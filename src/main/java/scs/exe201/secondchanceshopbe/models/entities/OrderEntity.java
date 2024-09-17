3bf044f8ab2db1dda5d6cf49cbb9de12c356743d
tohka-vo-tri <imchibitouka@gmail.com>
2024-09-17 23:57:44 +0700

    fix order entity

--- src/main/java/scs/exe201/secondchanceshopbe/models/entities/OrderEntity.java
+++ src/main/java/scs/exe201/secondchanceshopbe/models/entities/OrderEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "order")
+@Table(name ="order")
 public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
