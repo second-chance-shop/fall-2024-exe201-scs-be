8d49137a8b22ddaa7062f9ba760a05a7303480b8
ontrip <hoangtoc202@gmail.com>
2024-10-03 03:26:28 +0700

    add server

--- src/main/java/scs/exe201/secondchanceshopbe/configs/OpenApiConfig.java
+++ src/main/java/scs/exe201/secondchanceshopbe/configs/OpenApiConfig.java
@@ -1,19 +1,21 @@
 package scs.exe201.secondchanceshopbe.configs;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
+
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
+
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,8 +24,15 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("/");
+        server.setDescription("Default");
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info().title("Example API")
+
                         .version("1.0.0")
                         .description("This is a simple API example using OpenAPI 3.0"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
@@ -53,7 +62,6 @@ public class OpenApiConfig {
         config.setAllowedOriginPatterns(List.of("*"));  // Hoặc chỉ định  danh sách  domain cụ thể
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-
         source.registerCorsConfiguration("/**",  config);
         return  new  CorsFilter(source);
     }
