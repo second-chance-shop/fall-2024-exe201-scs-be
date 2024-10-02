3fb7c9307890016705c5c78e3edad3742c5f310e
ontrip <hoangtoc202@gmail.com>
2024-10-03 02:09:22 +0700

    fix notation

--- src/main/java/scs/exe201/secondchanceshopbe/repositories/ShippingRepository.java
+++ src/main/java/scs/exe201/secondchanceshopbe/repositories/ShippingRepository.java
@@ -2,8 +2,9 @@ package scs.exe201.secondchanceshopbe.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import scs.exe201.secondchanceshopbe.models.entities.ShippingEntity;
-
+@Repository
 public interface ShippingRepository extends JpaRepository<ShippingEntity,Long>{
     
 }
