ff93bbc10cb0fd4ec9609c005268b285110d15c0
ontrip <hoangtoc202@gmail.com>
2024-10-15 16:53:28 +0700

    teamplate send otp emai

---
+++ src/main/java/scs/exe201/secondchanceshopbe/services/Iplm/TestAPIService.java
@@ -0,0 +1,23 @@
+package scs.exe201.secondchanceshopbe.services.Iplm;
+
+import com.cloudinary.Cloudinary;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+@Service
+@RequiredArgsConstructor
+public class TestAPIService {
+    private final Cloudinary cloudinary;
+
+    public Map upload(MultipartFile file)  {
+        try{
+            Map data = this.cloudinary.uploader().upload(file.getBytes(), Map.of());
+            return data;
+        }catch (IOException io){
+            throw new RuntimeException("Image upload fail");
+        }
+    }
+}
