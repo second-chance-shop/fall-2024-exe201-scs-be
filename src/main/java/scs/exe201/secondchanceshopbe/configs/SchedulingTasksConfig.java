66ee79fb5479c472acade4dcff82c4e491d25315
ontrip <hoangtoc202@gmail.com>
2024-10-22 04:31:44 +0700

    fix update rating and follow value

--- src/main/java/scs/exe201/secondchanceshopbe/configs/SchedulingTasksConfig.java
+++ src/main/java/scs/exe201/secondchanceshopbe/configs/SchedulingTasksConfig.java
@@ -1,9 +1,13 @@
 package scs.exe201.secondchanceshopbe.configs;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+@Configuration
+@EnableScheduling
 public class SchedulingTasksConfig implements SchedulingConfigurer {
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
