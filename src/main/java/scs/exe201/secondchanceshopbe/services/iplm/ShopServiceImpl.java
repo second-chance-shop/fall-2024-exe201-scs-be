2ed0182e2d30d075cfc55f1088d9aa0a5a3a2f90
ontrip <hoangtoc202@gmail.com>
2024-10-30 21:07:48 +0700

    check

--- src/main/java/scs/exe201/secondchanceshopbe/services/iplm/ShopServiceImpl.java
+++ src/main/java/scs/exe201/secondchanceshopbe/services/iplm/ShopServiceImpl.java
@@ -128,7 +128,7 @@ public class ShopServiceImpl implements ShopService {
         shopEntity.setTypeShop(categoryEntity);
         shopEntity.setStatus(StatusEnum.ACTIVE);
         shopEntity.setDateCreate(LocalDate.now());
-        shopEntity.setValueFollow(0.0);
+        shopEntity.setValueFollow(0);
 
         if(imageShop !=null && !imageShop.isEmpty()) {
           var image=  fileDatabaseService.uploadFile(imageShop);
